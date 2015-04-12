1dc96da29fdb68e394c5679becebff5475bba886
Mark Keightley <mkeightley@Marks-MacBook-Pro.local>
2015-04-12 11:31:56 +0100

    Calculator as of the end of lecture 1

--- SwiftCalc/ViewController.swift
+++ SwiftCalc/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        
+    }
 }
-
