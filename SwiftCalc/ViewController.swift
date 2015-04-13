68ac689a85f80f7e6e278f8196a25c2e6e882313
Mark Keightley <mkeightley@Marks-MacBook-Pro.local>
2015-04-13 15:41:52 +0100

    Right, fixed the problem.  As I worked out, this is a bug, or at least it is a feature which was in Xcode 6.2, but which has been removed (or broken) in 6.3 which uses a different version of swift (1.2?)

--- SwiftCalc/ViewController.swift
+++ SwiftCalc/ViewController.swift
@@ -62,26 +62,26 @@ class ViewController: UIViewController {
         }
         
         switch operation {
-        case "+": performOperation { $0 + $1 }
-        case "−": performOperation { $1 - $0 }
-        case "×": performOperation { $0 * $1 }
-        case "÷": performOperation { $1 / $0 }
-        //case "√": performOperation { sqrt($0) }
+        case "+": performBinaryOperation { $0 + $1 }
+        case "−": performBinaryOperation { $1 - $0 }
+        case "×": performBinaryOperation { $0 * $1 }
+        case "÷": performBinaryOperation { $1 / $0 }
+        case "√": performUniaryOperation { sqrt($0) }
         default: break
         }
     }
     
-    func performOperation(operation: (Double, Double) -> Double) {
+    func performBinaryOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
             enter()
         }
     }
     
-//    func performOperation(operation: Double -> Double) {
-//        if operandStack.count >= 1 {
-//            displayValue = operation(operandStack.removeLast())
-//            enter()
-//        }
-//    }
+    func performUniaryOperation(operation: Double -> Double ) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
 }
