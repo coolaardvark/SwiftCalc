4cbc0d44388a00d43d2d7f3033e74f23084f69f1
Mark Keightley <mkeightley@Marks-MacBook-Pro.local>
2015-04-13 11:53:51 +0100

    Code as of end of lecture 2.  Still can't get the single operand button to work however!

--- SwiftCalc/ViewController.swift
+++ SwiftCalc/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)";
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var operandStack = Array<Double>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +46,42 @@ class ViewController: UIViewController {
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
         }
-        
     }
+    
+    @IBAction func enter() {
+        operandStack.append(displayValue)
+        userIsInTheMiddleOfTypingANumber = false
+        println("Operand stack = \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        
+        switch operation {
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        //case "√": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
+    }
+    
+//    func performOperation(operation: Double -> Double) {
+//        if operandStack.count >= 1 {
+//            displayValue = operation(operandStack.removeLast())
+//            enter()
+//        }
+//    }
 }
